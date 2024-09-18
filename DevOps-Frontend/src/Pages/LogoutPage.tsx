22e0f161799cb4028575032536a706703a2171e1
Yousra Diab <yousserah@gmail.com>
2024-09-18 18:25:08 +0200

    deleted log out

--- DevOps-Frontend/src/Pages/LogoutPage.tsx
+++ DevOps-Frontend/src/Pages/LogoutPage.tsx
@@ -1,7 +1,3 @@
 export default function LogoutPage() {
-  const { logout } = useAuth();
-  useEffect(() => {
-    logout();
-  }, [logout]);
-  return <Redirect to="/" />;
+
 }
--- DevOps-Frontend/src/Pages/SearchPage.tsx
+++ DevOps-Frontend/src/Pages/SearchPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import SearchComponent from '../Components/SearchComponent';
 import { getSearchResults } from '../api/api';
 
