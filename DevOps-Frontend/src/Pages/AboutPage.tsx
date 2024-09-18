a01c56cb6850bb1e698a9ddc7c09556da55b7c87
Yousra Diab <yousserah@gmail.com>
2024-09-18 20:40:02 +0200

    Dockerfile og docker compose fil

--- DevOps-Frontend/src/Pages/AboutPage.tsx
+++ DevOps-Frontend/src/Pages/AboutPage.tsx
@@ -1,13 +1,16 @@
-
+import { NavLink } from "react-router-dom";
 function AboutPage() {
   return (
+
     <div>
       <h1>Our mission</h1>
       <p>We intend to build the world's best search engine!</p>
 
       <h2>Our team</h2>
       
+      <NavLink to="/search">Search the Web</NavLink>
     </div>
+    
   );
 }
 
