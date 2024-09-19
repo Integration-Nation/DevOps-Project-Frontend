4e90ecf04bc0793691256d916153ca8d688424c5
AdamWarfa <Adamwarfadk@gmail.com>
2024-09-19 16:29:33 +0200

    post method for search

--- DevOps-Frontend/src/api/api.tsx
+++ DevOps-Frontend/src/api/api.tsx
@@ -1,12 +1,15 @@
+const endpoint = import.meta.env.VITE_ENDPOINT;
 
-const endpoint = import.meta.env.VITE_ENDPOINT
+async function getSearchResults(query: string, language: string = "en") {
+  const response = await fetch(`${endpoint}/api/search?q=${encodeURIComponent(query)}&language=${encodeURIComponent(language)}`, {
+    method: "POST", // Specify POST method
+  });
 
-async function getSearchResults(query: string, language: string = 'en') {
-  const response = await fetch(`${endpoint}/api/search?q=${encodeURIComponent(query)}&language=${encodeURIComponent(language)}`);
   if (!response.ok) {
-    throw new Error('Failed to fetch search results');
+    throw new Error("Failed to fetch search results");
   }
-  return response.json(); // Assuming the API returns a JSON object with 'search_results' key
+
+  return response.json(); // Assuming the API returns a JSON object
 }
 
 export { getSearchResults };
