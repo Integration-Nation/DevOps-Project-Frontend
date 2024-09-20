023391d5abafc2e57c5f8cfd7a9f882f23c8fba3
AdamWarfa <Adamwarfadk@gmail.com>
2024-09-20 02:16:38 +0200

    frontend routes done

--- DevOps-Frontend/src/App.tsx
+++ DevOps-Frontend/src/App.tsx
@@ -1,20 +1,21 @@
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import AboutPage from './Pages/AboutPage'
-import SearchPage from './Pages/SearchPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutPage from "./Pages/AboutPage";
+import SearchPage from "./Pages/SearchPage";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<AboutPage />} />
-          <Route path="/search" element={<SearchPage />} />
+          <Route path="/" element={<SearchPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/register" element={<h1>Register</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/weather" element={<h1>Weather</h1>} />
         </Routes>
       </BrowserRouter>
-      </>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
