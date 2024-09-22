9c52538db73af51855ae2e656bb5cfa4db5f208f
Yousra Diab <yousserah@gmail.com>
2024-09-22 15:24:42 +0200

    SCRUM-6-register

--- DevOps-Frontend/src/api/api.tsx
+++ DevOps-Frontend/src/api/api.tsx
@@ -1,5 +1,11 @@
 const endpoint = import.meta.env.VITE_ENDPOINT;
 
+export interface RegisterRequest {
+username: string,
+email: string,
+password: string,
+
+}
 async function getSearchResults(query: string, language: string = "en") {
   const response = await fetch(`${endpoint}/api/search?q=${encodeURIComponent(query)}&language=${encodeURIComponent(language)}`, {
     method: "POST", // Specify POST method
@@ -12,4 +18,23 @@ async function getSearchResults(query: string, language: string = "en") {
   return response.json(); // Assuming the API returns a JSON object
 }
 
-export { getSearchResults };
+//register users
+async function registerUser(registerRequest: RegisterRequest) {
+  const response = await fetch(`${endpoint}/api/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(registerRequest),
+    
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch register user");
+  }
+
+  return response.json();
+}
+
+
+export { getSearchResults, registerUser };
